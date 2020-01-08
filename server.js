b64c4f950758e176ff9de43215e386c30c76d730
realsep <58202232+realsep@users.noreply.github.com>
2020-01-08 16:05:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,49 +13,11 @@ app.get('/', function(req, res) {
     // res.sendFile('/search.html', { root: __dirname });
     // res.redirect('https://ff55bb19.ngrok.io');
 
-    res.send("Online");
+    res.sendFile('/searchliff.html', { root: __dirname });
 });
 app.post('/liff', function(req, res) {
 
-    // res.send("0000");
-    window.onload = function(e) {
-        liff.init(function(data) {
-            initializeApp(data);
-        });
-    };
-
-    function initializeApp(data) {
-        document.getElementById('send').addEventListener('click', function() {
-
-            // var msg = document.getElementById('uname').value,
-            //     var msg1 = document.getElementById('fdate').value,
-            //         var msg2 = document.getElementById('ldate').value
-
-
-
-            liff.sendMessages([{
-                Type: 'text',
-                text: "0000"
-                    // Type: 'text',
-                    // text: msg,
-                    // text: msg1,
-                    // text: msg2
-
-
-
-            }]).then(function() {
-                window.alert("Message sent");
-            }).catch(function(error) {
-                window.alert("Error sending message:" + error);
-            });
-        });
-    }
-
-
-
-
-
-
+    res.send("0000");
 });
 
 app.listen(process.env.PORT || port, function() {
