98440a3d844407745af7a5cc495e47c9d0f7298e
realsep <58202232+realsep@users.noreply.github.com>
2020-01-09 16:54:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Assessment = require('../app/models/assessment');
 
 const port = 5000;
 
 var app = new express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + process.env.PORT || port);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.get('/', function(req, res) {
     // res.sendFile('/search.html', { root: __dirname });
