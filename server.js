c10bb6cbef05ed19da11aafff87148e639e679d1
realsep <58202232+realsep@users.noreply.github.com>
2020-01-09 16:31:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
 const port = 5000;
 
 var app = new express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
