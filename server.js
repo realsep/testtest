7c05d466f304194cab57c0035d9180faf192900e
realsep <58202232+realsep@users.noreply.github.com>
2020-01-14 17:56:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 const port = 5000;
 
@@ -23,8 +24,27 @@ app.get('/', function(req, res, next) {
 //     res.sendFile('/searchliff.html', { root: __dirname });
 });
 app.post('/liff', function(req, res, next) {
-  res.send("0527");
- 
+// An object of options to indicate where to post to
+var post_options = {
+    url: 'https://gentle-crag-28693.herokuapp.com/search',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+};
+
+// Set up the request
+var post_req = http.request(post_options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('Response: ' + chunk);
+    });
+});
+
+// post the data
+post_req.write();
+post_req.end();
+    
 });
 
 app.listen(process.env.PORT || port, function() {
