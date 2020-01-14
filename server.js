5da680d85aee2a16548a86d7a273cfd3029bb833
realsep <58202232+realsep@users.noreply.github.com>
2020-01-14 13:51:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ var bodyParser = require('body-parser');
 const port = 5000;
 
 var app = new express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get('/', function(req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    });
+app.get('/', function(req, res, next) {
+    // res.sendFile('/search.html', { ro,ot: __dirname });
+    // res.redirect('https://ff55bb19.ngrok.io');
 
-    res.sendFile('/searchliff.html', { root: __dirname });
+//     res.sendFile('/searchliff.html', { root: __dirname });
 });
-app.post('/liff', function(req, res) {
-    res.send("0000");
-   
+app.post('/liff', function(req, res, next) {
+    res.send("00000");
 });
 
 app.listen(process.env.PORT || port, function() {
