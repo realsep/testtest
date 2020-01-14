58eed5ff2f0991dc562bed12b1fdf34a95803d46
realsep <58202232+realsep@users.noreply.github.com>
2020-01-14 16:18:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/', function(req, res) {
     // res.sendFile('/search.html', { root: __dirname });
     // res.redirect('https://ff55bb19.ngrok.io');
 
-    res.sendFile('/searchliff.html', { root: __dirname });
+//     res.sendFile('/searchliff.html', { root: __dirname });
 });
-app.post('/liff', function(req, res) {
+app.post('/liff', function(req, res) { //เรียก heroku gentle-crag-28693/search
 
     res.send("0000");
    
