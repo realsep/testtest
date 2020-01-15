5f7dbef627441c3d88214316ea195d8007db839a
realsep <58202232+realsep@users.noreply.github.com>
2020-01-15 09:31:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const port = 5000;
 var app = new express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use(bodyParser.json());
@@ -21,13 +21,30 @@ app.get('/', function(req, res, next) {
     // res.sendFile('/search.html', { ro,ot: __dirname });
     // res.redirect('https://ff55bb19.ngrok.io');
 
-//     res.sendFile('/searchliff.html', { root: __dirname });
+    //     res.sendFile('/searchliff.html', { root: __dirname });
 });
 app.post('/liff', function(req, res, next) {
-// An object of options to indicate where to post to
+    // An object of options to indicate where to post to
+    var post_options = {
+        url: 'https://gentle-crag-28693.herokuapp.com/search',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    };
 
-    
-  res.send("0527");
+    // Set up the request
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
+
 });
 
 app.listen(process.env.PORT || port, function() {
